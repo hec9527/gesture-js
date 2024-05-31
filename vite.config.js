72cc6baafe931bbe2cc082d28efb1599dfbc75ec
hec9527 <hec9527@foxmail.com>
2024-05-31 15:34:10 +0800

    feat: 添加类型申明

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      fileName: 'gesture',
+      formats: ['es', 'cjs', 'iife', 'umd'],
+      name: 'Gesture',
+    },
+  },
 });
